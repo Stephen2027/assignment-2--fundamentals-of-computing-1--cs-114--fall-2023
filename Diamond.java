3b63147de7704e4560ba2e862aeb08df6c8c5476
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-09-28 17:17:12 -0400

    Added question and place for input variable

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int a;
+    // asking for number input
+    System.out.println("Enter a number");
+    Scanner scan = new Scanner(System.in);
+    a = scan.nextInt();
+
   }
 }
