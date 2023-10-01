9a4d32d2300c9ae6ac577596fc2b3fe775ec6d75
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-10-01 17:00:30 -0400

    Added more variables and started on "for" loops

--- Diamond.java
+++ Diamond.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int a;
+    int a, b, row_nmbr;
     // asking for number input
     System.out.println("Enter a number");
     Scanner scan = new Scanner(System.in);
-    a = scan.nextInt();
+    row_nmbr = scan.nextInt();
 
     // generate diamond based on number inputed
+    for(a = 0 ;a <= row_nmbr ; a++); {
+      for(b = 1 ; b <= row_nmbr - 1 ; b++); {
+        System.out.print(" ");
+      }
+    }
   }
 }
