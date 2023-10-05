74736c8ff0f8410b41ff433d6c790829ab567113
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-10-05 15:58:38 -0400

    Finished adding in rows as well as fix errors that came up on lines 12 and 13

--- Diamond.java
+++ Diamond.java
@@ -2,17 +2,30 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int a, b, row_nmbr;
+    int a, b, rowNmbr;
     // asking for number input
     System.out.println("Enter a number");
     Scanner scan = new Scanner(System.in);
-    row_nmbr = scan.nextInt();
+    rowNmbr = scan.nextInt();
 
     // generate diamond based on number inputed
-    for(a = 0 ;a <= row_nmbr ; a++); {
-      for(b = 1 ; b <= row_nmbr - 1 ; b++); {
+    for(a = 0 ;a <= rowNmbr ; a++) {
+      for(b = 1 ; b <= rowNmbr - a ; b++) {
         System.out.print(" ");
       }
+      for(b = 1 ; b <= 2*a-1 ; b++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+    for(a = rowNmbr - 1 ; a >= 1 ; a--) {
+      for(b = 1 ; b <= rowNmbr - a ; b++) {
+        System.out.print(" ");
+      }
+      for (b = 1 ; b <= 2*a-1 ; b++) {
+        System.out.print("*");
+      }
+      System.out.println("");
     }
   }
 }
