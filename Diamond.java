bcdfe6beb182a9c9d7edf08e4174729b70d32136
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-10-14 21:47:23 -0400

    finish creating even diamond and edited odd if statement

--- Diamond.java
+++ Diamond.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int rows, diamond, shapeSize, space;
+    int rows, diamond, shapeSize;
     // asking for number input
     System.out.println("Enter a number");
     Scanner scan = new Scanner(System.in);
     shapeSize = scan.nextInt();
 
     // Create top for diamond odd
-    if(shapeSize > 2 && shapeSize % 2 != 0);{
+    if(shapeSize %2 == 1){
         for (rows = 1; rows < shapeSize; rows += 2){
             for (diamond = 0; diamond < shapeSize - 1 - rows / 2; diamond++) {
                 System.out.print(" ");
@@ -22,7 +22,7 @@ public class Diamond {
 
     // Create bottom of diamond for odd
         for (rows = shapeSize; rows > 0; rows -= 2){
-            for ( diamond = 0; diamond < shapeSize -1 - rows / 2; diamond++) {
+            for ( diamond = 0; diamond < shapeSize - 1 - rows / 2; diamond++) {
                 System.out.print(" ");
         }
         for ( diamond = 0; diamond < rows; diamond++) {
@@ -30,22 +30,39 @@ public class Diamond {
         }
         System.out.println("");
     }
-    scan.close();
     }
     // Create top of diamond for even
-    if (shapeSize > 2 && shapeSize % 2 != 1); {
-        for (rows = 1; rows > shapeSize; ++rows ){
-            for (space = 0; space < shapeSize; ++space){
+    else if (shapeSize %2 == 0) {
+      for (int spaces = shapeSize - 1; spaces > 0; spaces -= 1 ){
+        System.out.print(" ");
+      }
+      System.out.println(" *");
+        for (rows = 2; rows <= shapeSize/2 + 1; rows += 1){
+            for (int spaces = -2 * rows + (shapeSize + 2); spaces > 0; spaces -= 1){ // makes sure that there is the correct number of spaces printed
                 System.out.print(" ");
             }
-            for (diamond = 1; diamond < rows; ++diamond){
-                System.out.print("*");
+            for (diamond = 2 * rows - 2; diamond > 0; diamond -= 1){
+                System.out.print(" *");
             }
             System.out.println("");
         }
-        for (rows = shapeSize; rows > 0; --rows){
+        // Create bottom of diamond for even
+        for (rows = shapeSize/2; rows >= 2; rows--){
+          for (int spaces = -2 * rows + (shapeSize + 2); spaces > 0; spaces -= 1){
+            System.out.print(" ");
+          }
+          for(diamond = 2 * rows - 2; diamond > 0; diamond -=1){
+            System.out.print(" *");
+          }
+          System.out.println("");
 
         }
+        for (int spaces = shapeSize - 1; spaces > 0; spaces -= 1){
+          System.out.print(" ");
+        }
+        System.out.println(" *");
+        scan.close();
     }
+
   }
 }
