b694a5791ca639b3d491ec646ea6e596504aad6d
Stephen2027 <143655666+Stephen2027@users.noreply.github.com>
2023-10-16 00:15:22 -0400

    Added comments and edited previous comments

--- Diamond.java
+++ Diamond.java
@@ -11,10 +11,10 @@ public class Diamond {
     // Create top for diamond odd
     if(shapeSize %2 == 1){
         for (rows = 1; rows < shapeSize; rows += 2){
-            for (diamond = 0; diamond < shapeSize - 1 - rows / 2; diamond++) {
+            for (diamond = 0; diamond < shapeSize - 1 - rows / 2; diamond++) {// print correct number of rows up to the middle
                 System.out.print(" ");
         }
-        for (diamond = 0; diamond < rows; diamond++) {
+        for (diamond = 0; diamond < rows; diamond++) { // print correct number of * for each of the top row
             System.out.print("*");
         }
         System.out.println("");
@@ -23,33 +23,36 @@ public class Diamond {
     // Create bottom of diamond for odd
         for (rows = shapeSize; rows > 0; rows -= 2){
             for (diamond = 0; diamond < shapeSize - 1 - rows / 2; diamond++) {
+              // print the correct number of rows from middle to the bottom
                 System.out.print(" ");
         }
-        for (diamond = 0; diamond < rows; diamond++) {
+        for (diamond = 0; diamond < rows; diamond++) { // prints correct number of * for each bottom row
             System.out.print("*");
         }
         System.out.println("");
     }
     }
-    // Create top of diamond for even
+    // Top row of diamond for even
     else if (shapeSize %2 == 0) {
-      for (int spaces = shapeSize - 1; spaces > 0; spaces -= 1 ){ // creates correct number of spaces for first row
+      for (int spaces = shapeSize - 1; spaces > 0; spaces -= 1 ){
+        // creates correct number of spaces for first row and prints correct number of " *"
         System.out.print(" ");
       }
       // Middle rows for top of even diamond
       System.out.println(" *");
         for (rows = 2; rows <= shapeSize/2 + 1; rows += 1){
-            for (int spaces = -2 * rows + (shapeSize + 2); spaces > 0; spaces -= 1){ // prints the correct number of spaces before the " *" is printed
+            for (int spaces = -2 * rows + (shapeSize + 2); spaces > 0; spaces -= 1){
+              // prints the correct number of spaces before " *" is printed
                 System.out.print(" ");
             }
-            for (diamond = 2 * rows - 2; diamond > 0; diamond -= 1){
+            for (diamond = 2 * rows - 2; diamond > 0; diamond -= 1){ // prints correct amount of " *" for each row
                 System.out.print(" *");
             }
             System.out.println("");
         }
-        // Create middle rows for bottom of diamond for even
+        // Middle rows for bottom of diamond for even
         for (rows = shapeSize/2; rows >= 2; rows--){
-          for (int spaces = -2 * rows + (shapeSize + 2); spaces > 0; spaces -= 1){
+          for (int spaces = -2 * rows + (shapeSize + 2); spaces > 0; spaces -= 1){ // prints correct spaces for before each " *"
             System.out.print(" ");
           }
           for(diamond = 2 * rows - 2; diamond > 0; diamond -=1){ // prints the correct number of " *" for each row
@@ -58,8 +61,9 @@ public class Diamond {
           System.out.println("");
 
         }
-        // Create last row for bottom of even numbers
+        // Last row for bottom of even numbers
         for (int spaces = shapeSize - 1; spaces > 0; spaces -= 1){
+          // creates correct number of spaces for last row then prints correct amount of " *" for that row
           System.out.print(" ");
         }
         System.out.println(" *");
